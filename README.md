6b32c727413cfdd58761524f7e3ec39fb2e59ebd
vangeest <49232516+vangeest@users.noreply.github.com>
2021-03-21 17:36:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+Deze game is gebaseerd op het template voor 4HV van het Emmauscollege Rotterdam
+
+## Mijn spel: *naam van het spel*
+Gemaakt door:
+- *naam van leerling 1*
+- *naam van leerling 2*
+
+## Mijn planning
+
+Basis: uitleg, spelen, afgaan en punten
+- [x] maak index.html, style.css en script.js met canvas
+- [ ] teken speler
+- [ ] beweeg speler
+- [ ] ... *(vul zelf aan)*
+- [ ] punten op scherm zetten
+- [ ] punten kunnen scoren
+- [ ] uitlegscherm
+
+Uitbreiding: *bedenk iets, bijvoorbeeld meer tegenstanders*
+- [ ] *later uitwerken*
+- [ ] *later uitwerken*
+- [ ] *later uitwerken*
+
+Uitbreiding: *bedenk iets, bijvoorbeeld bewegende achtergrond*
+- [ ] *later uitwerken*
+- [ ] *later uitwerken*
+- [ ] *later uitwerken*
+
+## Credits
+- Game template van het Emmauscollege Rotterdam https://github.com/emmauscollege/4HV-game-template
+- manifest.json https://codelabs.developers.google.com/codelabs/your-first-pwapp/#3
+- icon http://www.iconarchive.com/show/android-lollipop-icons-by-dtafalonso/Play-Games-icon.html
+- ...
---
+++ script.js
@@ -0,0 +1,189 @@
+/// @ts-check
+/// <reference path=".gitpod/p5.global-mode.d.ts" />
+"use strict";
+
+/* Game opdracht
+   Informatica - Emmauscollege Rotterdam
+   Template voor een game in JavaScript met de p5 library
+
+   Begin met dit template voor je game opdracht,
+   voeg er je eigen code aan toe.
+ */
+
+
+
+
+/* ********************************************* */
+/* globale variabelen die je gebruikt in je game */
+/* ********************************************* */
+
+const UITLEG = 0;
+const SPELEN = 1;
+const GAMEOVER = 2;
+var spelStatus = SPELEN;
+
+var spelerX = 200; // x-positie van speler
+var spelerY = 100; // y-positie van speler
+
+var kogelX = 0;    // x-positie van kogel
+var kogelY = 0;    // y-positie van kogel
+
+var vijandX = 0;   // x-positie van vijand
+var vijandY = 0;   // y-positie van vijand
+
+var score = 0; // aantal behaalde punten
+
+
+
+
+
+/* ********************************************* */
+/*      functies die je gebruikt in je game      */
+/* ********************************************* */
+
+
+/**
+ * Tekent het speelveld
+ */
+var tekenVeld = function () {
+  fill("purple");
+  rect(20, 20, width - 2 * 20, height - 2 * 20);
+};
+
+
+/**
+ * Tekent de vijand
+ * @param {number} x x-coördinaat
+ * @param {number} y y-coördinaat
+ */
+var tekenVijand = function(x, y) {
+    
+
+};
+
+
+/**
+ * Tekent de kogel of de bal
+ * @param {number} x x-coördinaat
+ * @param {number} y y-coördinaat
+ */
+var tekenKogel = function(x, y) {
+
+
+};
+
+
+/**
+ * Tekent de speler
+ * @param {number} x x-coördinaat
+ * @param {number} y y-coördinaat
+ */
+var tekenSpeler = function(x, y) {
+  fill("white");
+  ellipse(x, y, 50, 50);
+};
+
+
+/**
+ * Updatet globale variabelen met positie van vijand of tegenspeler
+ */
+var beweegVijand = function() {
+    
+};
+
+
+/**
+ * Updatet globale variabelen met positie van kogel of bal
+ */
+var beweegKogel = function() {
+
+};
+
+
+/**
+ * Kijkt wat de toetsen/muis etc zijn.
+ * Updatet globale variabele spelerX en spelerY
+ */
+var beweegSpeler = function() {
+
+};
+
+
+/**
+ * Zoekt uit of de vijand is geraakt
+ * @returns {boolean} true als vijand is geraakt
+ */
+var checkVijandGeraakt = function() {
+
+  return false;
+};
+
+
+/**
+ * Zoekt uit of de speler is geraakt
+ * bijvoorbeeld door botsing met vijand
+ * @returns {boolean} true als speler is geraakt
+ */
+var checkSpelerGeraakt = function() {
+    
+  return false;
+};
+
+
+/**
+ * Zoekt uit of het spel is afgelopen
+ * @returns {boolean} true als het spel is afgelopen
+ */
+var checkGameOver = function() {
+    
+  return false;
+};
+
+
+/**
+ * setup
+ * de code in deze functie wordt één keer uitgevoerd door
+ * de p5 library, zodra het spel geladen is in de browser
+ */
+function setup() {
+  // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
+  createCanvas(1280, 720);
+
+  // Kleur de achtergrond blauw, zodat je het kunt zien
+  background('blue');
+}
+
+
+/**
+ * draw
+ * de code in deze functie wordt meerdere keren per seconde
+ * uitgevoerd door de p5 library, nadat de setup functie klaar is
+ */
+function draw() {
+  switch (spelStatus) {
+    case SPELEN:
+      beweegVijand();
+      beweegKogel();
+      beweegSpeler();
+      
+      if (checkVijandGeraakt()) {
+        // punten erbij
+        // nieuwe vijand maken
+      }
+      
+      if (checkSpelerGeraakt()) {
+        // leven eraf of gezondheid verlagen
+        // eventueel: nieuwe speler maken
+      }
+
+      tekenVeld();
+      tekenVijand(vijandX, vijandY);
+      tekenKogel(kogelX, kogelY);
+      tekenSpeler(spelerX, spelerY);
+
+      if (checkGameOver()) {
+        spelStatus = GAMEOVER;
+      }
+      break;
+  }
+}
