08fdcc7e7e18f3c855f381b272ba1a67da2bf552
AnthonyHit <112371@emmauscollege.nl>
2021-03-29 08:33:20 +0000

    Speler x,y
    Signed-off-by: AnthonyHit <112371@emmauscollege.nl>

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ const SPELEN = 1;
 const GAMEOVER = 2;
 var spelStatus = SPELEN;
 
-
+var spelerX = 50; // x-positie van speler
+var spelerY = 600; // y-positie van speler
 
 var kogelX = 0;    // x-positie van kogel
 var kogelY = 0;    // y-positie van kogel
@@ -77,15 +78,10 @@ var tekenKogel = function(x, y) {
  * @param {number} x x-coördinaat
  * @param {number} y y-coördinaat
  */
-var spelerX = 300
-var spelerY = 500 
 var tekenSpeler = function(x, y) {
   fill("white");
-  rect(x, y, 50, 50);
-
-  var spelerX = 1300; // x-positie van speler
-var spelerY = 1400; // y-positie van speler
-}; 
+  rect(spelerX, spelerY, 50, 50);
+};
 
 
 /**
